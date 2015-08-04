2fb37aa97874730a3e6e129dca4412c879e23b41
Darren Mulholland <dmulholland@outlook.ie>
2015-08-05 00:34:13 +0100

    Refactor Go code and comments for readability

--- go/hexdump.go
+++ go/hexdump.go
@@ -1,5 +1,5 @@
 /*
-    Hex dump command line utility.
+    Hexdump command line utility.
 
       * Author: Darren Mulholland <dmulholland@outlook.ie>
       * License: Public Domain
@@ -16,9 +16,11 @@ import (
 )
 
 
-const version = "0.2.0"
+// Application version number.
+const version = "0.3.0"
 
 
+// Command line help text.
 const usage = `Usage: godump [FLAGS] [OPTIONS] ARGUMENTS
 
 Arguments:
@@ -34,22 +36,30 @@ Flags:
   --version  display version number and exit`
 
 
+// Write a single line of output to stdout.
 func writeln(buffer []byte, numBytes int, offset int, bytesPerLine int) {
 
+    // Write the line number.
     fmt.Printf("% 6X |", offset)
 
     for i := 0; i < bytesPerLine; i++ {
+
+        // Write an extra space in front of every fourth byte except the first.
+        if i > 0 && i % 4 == 0 {
+            fmt.Printf(" ")
+        }
+
+        // Write the byte in hex form, or a spacer if we're out of bytes.
         if i < numBytes {
             fmt.Printf(" %02X", buffer[i])
         } else {
             fmt.Printf("   ")
         }
-        if (i + 1) % 4 == 0 && i != bytesPerLine - 1 {
-            fmt.Printf(" ")
-        }
     }
 
     fmt.Printf(" | ")
+
+    // Write a character for each byte in the printable ascii range.
     for i, b := range buffer {
         if i < numBytes {
             if b >= 32 && b <= 126 {
@@ -59,12 +69,15 @@ func writeln(buffer []byte, numBytes int, offset int, bytesPerLine int) {
             }
         }
     }
+
     fmt.Printf("\n")
 }
 
 
+// Dump the specified file to stdout.
 func dump(file *os.File, offset int, bytesToRead int, bytesPerLine int) {
 
+    // If an offset has been specified, attempt to seek to it.
     if offset != 0 {
         _, err := file.Seek(int64(offset), 0)
         if err != nil {
@@ -73,8 +86,10 @@ func dump(file *os.File, offset int, bytesToRead int, bytesPerLine int) {
         }
     }
 
+    // Allocate a buffer to hold one line of input from the file.
     buffer := make([]byte, bytesPerLine)
 
+    // Read and dump one line of input per iteration.
     for {
         if bytesToRead > -1 && bytesToRead < bytesPerLine {
             buffer = make([]byte, bytesToRead)
@@ -95,31 +110,40 @@ func dump(file *os.File, offset int, bytesToRead int, bytesPerLine int) {
 }
 
 
+// Application entry point.
 func main() {
 
+    // Command line options.
     var offset = flag.Int("o", 0, "offset into file")
     var bytesToRead = flag.Int("n", -1, "number of bytes to read")
     var bytesPerLine = flag.Int("l", 16, "bytes per line in output")
+
+    // Command line flags.
     var printHelp = flag.Bool("help", false, "print help and exit")
     var printVersion = flag.Bool("version", false, "print version and exit")
 
+    // Called if an error occurs while parsing flags.
     flag.Usage = func() {
         fmt.Println()
         fmt.Println(usage)
     }
 
+    // Parse the command line arguments.
     flag.Parse()
 
+    // Check if the --help flag is present.
     if *printHelp {
         fmt.Println(usage)
         os.Exit(0)
     }
 
+    // Check if the --version flag is present.
     if *printVersion {
         fmt.Println(version)
         os.Exit(0)
     }
 
+    // Default to reading from stdin if no filename has been specified.
     if flag.NArg() > 0 {
         file, err := os.Open(flag.Arg(0))
         if err != nil {
